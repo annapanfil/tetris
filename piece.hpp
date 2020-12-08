da25d9f5c7c99a88b254e89f889c6bd1c675545b
panka134 <134panka@gmail.com>
2020-12-08 11:14:34 +0100

    move piece

--- piece.hpp
+++ piece.hpp
@@ -5,13 +5,14 @@ class Piece: public Shape, public sf::Drawable{
     vector<vector<bool>> shape;
     vector<vector<bool>> random_shape();
     int PIXEL;
+    void move(int dx, int dy);
 public:
     vector<sf::RectangleShape*> visual;
     Piece(int x, int y, const int PIXEL); //TODO: from board
-  // bool wall_collision();
-  // bool stack_collision(Stack stack);
-  void move_horizontally();
-  void move_down();
-  void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
-  // void rotate(char direction);
+    // bool wall_collision();
+    // bool stack_collision(Stack stack);
+    void move_horizontally();
+    void move_down();
+    void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
+    // void rotate(char direction);
 };
