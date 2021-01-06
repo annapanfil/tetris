ad8a4fa42b6f54366eb74f07e18c45070a50b65e
Anna Panfil <55759533+panka134@users.noreply.github.com>
2021-01-06 14:33:28 +0100

    Game (#6)
    * show rectangle
    
    * show l_piece
    
    * refactor
    
    - change arrays to vectors
    - move l_piece to piece
    - random_shape()
    
    * move_down skeleton
    
    * movement - scratch
    
    * move piece
    
    * wall detection
    
    * rotate pieces
    
    * add game class, divide to functions
    
    * controls

--- game.hpp
+++ game.hpp
@@ -1,15 +1,21 @@
 #pragma once
+#include "board.hpp"
+#include "piece.hpp"
+
 class Game{
-  board Board;
+  sf::RenderWindow* window;
+  Board* board;
+  Piece* piece;
   int score = 0;
   int speed;
-  enum controls {LEFT, RIGHT, UP, DOWN};
-  // keys[4]
-  bool paused = False;
+  bool paused = false;
 public:
+  Game();
   void start();
-  void pause();
-  void end();
-  void increase_speed();
+  void update();
+  // void pause();
+  // void end();
+  // void increase_speed();
+  void event_handling();
   void increment_score(int points=1){score += points;}
 };
--- piece.hpp
+++ piece.hpp
@@ -1,18 +1,20 @@
 #pragma once
 #include "shape.hpp"
+#include <iostream>
 
 class Piece: public Shape, public sf::Drawable{
     vector<vector<bool>> shape;
     vector<vector<bool>> random_shape();
     void move(int dx, int dy);
 public:
+    ~Piece() {std::cout << "destructor" << '\n';}
     vector<sf::RectangleShape*> visual;
     Piece(int x, int y, Board* Board);
     bool wall_collision();
     // bool stack_collision(Stack stack);
-    void move_horizontally();
+    void move_horizontally(int direction);
     void move_down();
     void draw(sf::RenderTarget& target, sf::RenderStates state) const override;
-    void rotate_clockwise(); //char direction
-    void rotate_counterclockwise()
+    void rotate_clockwise();
+    void rotate_counterclockwise();
 };
