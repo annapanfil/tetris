358a1cc0a604636c232c44837a7e894d0d127113
panka134 <134panka@gmail.com>
2021-01-21 11:45:46 +0100

    game over

--- game.cpp
+++ game.cpp
@@ -1,11 +1,14 @@
 #include "game.hpp"
 
+class GameOver: public std::exception{};
+
 Game::Game(){
   const int PIXEL = 20;
   this -> window = new sf::RenderWindow (sf::VideoMode(400,600), "Tetris", sf::Style::Close | sf::Style::Titlebar); //TODO: from board dimensions //sf::Style::Close || sf::Style::Titlebar
   this -> board = new Board (400, 600, PIXEL);
   this -> piece = new Piece(this->board);
   this -> stack = new Stack(this->board);
+  this -> paused = false;
 }
 
 void Game::display_text(sf::Font* font, std::string msg){
@@ -23,12 +26,16 @@ void Game::introduce(){
     std::cout<<"Font error";
   for (int i=3; i>=0; i--){
     display_text(&font, "Hello!\nGame starts in\n" + std::to_string(i));
-    sf::sleep(sf::seconds(1));
+    sf::sleep(sf::seconds(10));
   }
 }
 
 void Game::finish(){
-
+  sf::Font font;
+  if (!font.loadFromFile("graphics/font.ttf"))
+    std::cout<<"Font error";
+  display_text(&font, "GAME OVER\n");
+  throw GameOver();
 }
 
 void Game::update(){
@@ -37,8 +44,10 @@ void Game::update(){
       delete this->piece;
       this->piece = new Piece(this->board);
   }
-  if (stack->get_border() <=0 )
+  if (stack->get_border() <= 20){
+    std::cout<<"game over\n";
     finish();
+  }
 }
 
 void Game::event_handling(){
@@ -69,6 +78,25 @@ void Game::event_handling(){
   }
 }
 
+void Game::pause_menu(sf::RenderWindow* window){
+  sf::Event event;
+  while (window -> pollEvent(event) && paused == true){
+    //process events
+    std::cout<<"paused"<<std::endl; //żeby się 2h nie zastanawiać
+    switch(event.type){
+      case sf::Event::Closed:
+        window -> close(); break;
+      case sf::Event::KeyPressed:
+        if (event.key.code == sf::Keyboard::P || event.key.code == sf::Keyboard::Escape){
+          paused = false;
+          std::cout<<"unpause\n";
+        }
+      default: break;
+    }
+  }
+}
+
+
 void Game::start(){
   sf::Clock game_clock;
   sf::Clock key_clock;
@@ -78,31 +106,30 @@ void Game::start(){
   window -> clear(); //można podać kolor
   //game loop
   while (window->isOpen()){
-    if (!paused){
-    while (game_clock.getElapsedTime() < sf::seconds(fall_time)){
-      event_handling();
-      window -> clear();
-      window -> draw(*piece);
-      window -> draw(*stack);
-      window -> display();
+    try{
+      if (!paused){
+      while (game_clock.getElapsedTime() < sf::seconds(fall_time)){
+        event_handling();
+        window -> clear();
+        window -> draw(*piece);
+        window -> draw(*stack);
+        window -> display();
+      }
+      update();
+      game_clock.restart();
+      }
+      else{
+        pause_menu(window);
+      }
     }
-    update();
-    game_clock.restart();
-    }
-    else{
-      // std::cout<<"paused"<<std::endl;
+    catch(GameOver){
+      std::cout<<"caught\n";
       sf::Event event;
-      while (window -> pollEvent(event)){
-        //process events
-        std::cout<<"paused"<<std::endl; //żeby się 2h nie zastanawiać
-        switch(event.type){
-          case sf::Event::Closed:
-            window -> close(); break;
-          case sf::Event::KeyPressed:
-            if (event.key.code == sf::Keyboard::P || event.key.code == sf::Keyboard::Escape){
-              paused = false;
-            }
-          default: break;
+      while (window->isOpen()){
+        window -> pollEvent(event);
+        if(event.type == sf::Event::Closed){
+          std::cout<<"waiting\n";
+          window -> close(); break;
         }
       }
     }
