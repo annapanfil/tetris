aa839f011178d4da0bc8f84f3752e05012ceac37
panka134 <134panka@gmail.com>
2021-01-21 10:20:04 +0100

    stack horizontal collisions

--- game.cpp
+++ game.cpp
@@ -50,15 +50,11 @@ void Game::event_handling(){
         window -> close(); break;
       case sf::Event::KeyPressed:
         if (event.key.code == sf::Keyboard::Left){
-          std::cout<<"LEFT\n";
           piece->move_horizontally(-1, stack);
-          
-          // else
-          //   std::cout<<"NOT COLLISION\n";
-        }//TODO: check stack collision
-        else if (event.key.code == sf::Keyboard::Right)
+        }
+        else if (event.key.code == sf::Keyboard::Right){
           piece->move_horizontally(1, stack);
-          //TODO: check stack collision;
+        }
         else if (event.key.code == sf::Keyboard::X || event.key.code == sf::Keyboard::Up)
           piece->rotate_clockwise();
         else if (event.key.code == sf::Keyboard::Z)
